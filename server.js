b3cc4ea870e99702b760cd7a29aa76d24815a964
Shashi Kashyap <shashi.aimit@hotmail.com>
2018-07-04 23:53:39 +0530

    add signin and register apis

--- server.js
+++ server.js
@@ -1,9 +1,55 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.json());
+
+const data = {
+  users: [
+    {
+      id: 123,
+      name: 'John',
+      email: 'john@gmail.com',
+      password: 'password',
+      entries: 0,
+      joined: new Date()
+    },
+    {
+      id: 124,
+      name: 'David',
+      email: 'david@gmail.com',
+      password: '123456',
+      entries: 0,
+      joined: new Date()
+    }
+  ]
+}
 
 app.get('/', (req, res) => {
-  res.send('this is working');
+  res.json(data.users);
+});
+
+app.post('/signin', (req, res) => {
+  if (req.body.email === data.users[0].email && req.body.password === data.users[0].password) {
+    res.json('success');
+  } else {
+    res.status(400).json('error logging in');
+  }
+});
+
+app.post('/register', (req, res) => {
+  const { name, email, password } = req.body
+
+  data.users.push({
+    id: 125,
+    name: name,
+    email: email,
+    password: password,
+    entries: 0,
+    joined: new Date()
+  });
+
+  res.json(data.users[data.users.length - 1]);
 });
 
 app.listen(3000, () => {
