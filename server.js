1543640aff4c6f17053783bc77642a905c8908ce
Shashi Kashyap <shashi.aimit@hotmail.com>
2018-07-11 14:05:01 +0530

    finish signin with db

--- server.js
+++ server.js
@@ -45,26 +45,55 @@ app.get('/', (req, res) => {
 });
 
 app.post('/signin', (req, res) => {
-  if (req.body.email === data.users[0].email && req.body.password === data.users[0].password) {
-    res.json(data.users[0]);
-  } else {
-    res.status(400).json('error logging in');
-  }
+  const { email, password } = req.body
+  db.select('email', 'hash').from('login')
+    .where('email', '=', email)
+    .then(data => {
+      const isValid = bcrypt.compareSync(password, data[0].hash);
+
+      if (isValid) {
+        return db.select('*').from('users')
+          .where('email', '=', email)
+          .then(user => {
+            console.log(user);
+            res.json(user[0]);
+          })
+          .catch(err => res.status(400).json('unable to get user'));
+      } else {
+        res.status(400).json('wrong credentials');
+      }
+    })
+    .catch(err => res.status(400).json('wrong credentials'));
 });
 
 app.post('/register', (req, res) => {
   const { name, email, password } = req.body
+  const hash = bcrypt.hashSync(password);
 
-  db('users')
-    .returning('*')
-    .insert({
-      email: email,
-      name: name,
-      joined: new Date()
+  db.transaction(trx => {
+    trx.insert({
+      hash: hash,
+      email: email
     })
-    .then(user => {
-      res.json(user[0]);
+    .into('login')
+    .returning('email')
+    .then(loginEmail => {
+      return trx('users')
+        .returning('*')
+        .insert({
+          email: loginEmail[0],
+          name: name,
+          joined: new Date()
+        })
+        .then(user => {
+          res.json(user[0]);
+        })
     })
+    .then(trx.commit)
+    .catch(trx.rollback)
+  })
+
+
     .catch(err => res.status(400).json('unable to register'))
 
 });
