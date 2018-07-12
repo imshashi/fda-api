33ca218dfa393c847b4f6efb99d01a4b9c75752b
Shashi Kashyap <shashi.aimit@hotmail.com>
2018-07-12 23:57:57 +0530

    code cleanup - extract out register feature

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const cors = require('cors');
 const bcrypt = require('bcrypt-nodejs');
 const knex = require('knex');
 
+const register = require('./controllers/register');
+
 const db = knex({
   client: 'pg',
   connection: {
@@ -66,37 +68,7 @@ app.post('/signin', (req, res) => {
     .catch(err => res.status(400).json('wrong credentials'));
 });
 
-app.post('/register', (req, res) => {
-  const { name, email, password } = req.body
-  const hash = bcrypt.hashSync(password);
-
-  db.transaction(trx => {
-    trx.insert({
-      hash: hash,
-      email: email
-    })
-    .into('login')
-    .returning('email')
-    .then(loginEmail => {
-      return trx('users')
-        .returning('*')
-        .insert({
-          email: loginEmail[0],
-          name: name,
-          joined: new Date()
-        })
-        .then(user => {
-          res.json(user[0]);
-        })
-    })
-    .then(trx.commit)
-    .catch(trx.rollback)
-  })
-
-
-    .catch(err => res.status(400).json('unable to register'))
-
-});
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) });
 
 app.get('/profile/:id', (req, res) => {
   const { id } = req.params;
