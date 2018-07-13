669ba5d6f2d5c3bbedef1847f3a476126b13d8c5
Shashi Kashyap <shashi.aimit@hotmail.com>
2018-07-13 11:33:40 +0530

    add clarifai api call to backend

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,19 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+  apiKey: 'af9d1986e1b4481a8f92d6328ff3f8fe'
+});
+
+const handleApiCall = (req, res) => {
+  app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => res.status(400).json('unable to work with API'));
+}
+
+
 const handleImage = (req, res, db) => {
   const { id } = req.body;
 
@@ -12,5 +28,6 @@ const handleImage = (req, res, db) => {
 }
 
 module.exports = {
-  handleImage: handleImage
+  handleImage: handleImage,
+  handleApiCall: handleApiCall
 }
